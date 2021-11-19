4885576d61c1c628e5418abc3bd9888dfbf1081d
wookoo <nanayagoon@daum.net>
2021-11-19 19:31:42 +0900

    비밀번호 찾기 구현중

--- DataBase_Movie/FormFindPassword.Designer.cs
+++ DataBase_Movie/FormFindPassword.Designer.cs
@@ -29,12 +29,155 @@ namespace DataBase_Movie
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.send_auth_mail = new System.Windows.Forms.Button();
+            this.rePasswordBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.authCodeBox = new System.Windows.Forms.TextBox();
+            this.eMailBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.resetBtn);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.send_auth_mail);
+            this.panel1.Controls.Add(this.rePasswordBox);
+            this.panel1.Controls.Add(this.passwordBox);
+            this.panel1.Controls.Add(this.authCodeBox);
+            this.panel1.Controls.Add(this.eMailBox);
+            this.panel1.Location = new System.Drawing.Point(180, 109);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(447, 220);
+            this.panel1.TabIndex = 1;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(110, 178);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(246, 20);
+            this.resetBtn.TabIndex = 14;
+            this.resetBtn.Text = "비밀번호 변경";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "비밀번호 확인";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "새 비밀번호";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "인증번호";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "이메일";
+            // 
+            // send_auth_mail
+            // 
+            this.send_auth_mail.Location = new System.Drawing.Point(265, 59);
+            this.send_auth_mail.Name = "send_auth_mail";
+            this.send_auth_mail.Size = new System.Drawing.Size(91, 21);
+            this.send_auth_mail.TabIndex = 5;
+            this.send_auth_mail.Text = "인증번호 전송";
+            this.send_auth_mail.UseVisualStyleBackColor = true;
+            // 
+            // rePasswordBox
+            // 
+            this.rePasswordBox.Location = new System.Drawing.Point(109, 136);
+            this.rePasswordBox.MaxLength = 15;
+            this.rePasswordBox.Name = "rePasswordBox";
+            this.rePasswordBox.PasswordChar = '*';
+            this.rePasswordBox.Size = new System.Drawing.Size(247, 21);
+            this.rePasswordBox.TabIndex = 3;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(110, 99);
+            this.passwordBox.MaxLength = 15;
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.PasswordChar = '*';
+            this.passwordBox.Size = new System.Drawing.Size(246, 21);
+            this.passwordBox.TabIndex = 2;
+            // 
+            // authCodeBox
+            // 
+            this.authCodeBox.CausesValidation = false;
+            this.authCodeBox.Enabled = false;
+            this.authCodeBox.Location = new System.Drawing.Point(110, 56);
+            this.authCodeBox.MaxLength = 6;
+            this.authCodeBox.Name = "authCodeBox";
+            this.authCodeBox.ReadOnly = true;
+            this.authCodeBox.Size = new System.Drawing.Size(149, 21);
+            this.authCodeBox.TabIndex = 11;
+            // 
+            // eMailBox
+            // 
+            this.eMailBox.Location = new System.Drawing.Point(110, 18);
+            this.eMailBox.MaxLength = 50;
+            this.eMailBox.Name = "eMailBox";
+            this.eMailBox.Size = new System.Drawing.Size(246, 21);
+            this.eMailBox.TabIndex = 0;
+            // 
+            // FormFindPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormFindPassword";
             this.Text = "FormFindPassword";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button send_auth_mail;
+        private System.Windows.Forms.TextBox rePasswordBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.TextBox authCodeBox;
+        private System.Windows.Forms.TextBox eMailBox;
     }
 }
--- DataBase_Movie/FormFindPassword.cs
+++ DataBase_Movie/FormFindPassword.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
 namespace DataBase_Movie
 {
     public partial class FormFindPassword : Form
     {
+        int process = 0;
+        String code = "";
+        OleDbConnection conn;
+        string connectionString = "Provider=MSDAORA;Password=dohyun;User ID=dohyun"; //oracle 서버 연결
         public FormFindPassword()
         {
             InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void send_auth_btn_Click(object sender, EventArgs e)
+        {
+            String email = eMailBox.Text.Trim();
+            bool valid = Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/s=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+
+            if (!valid)
+            {
+                MessageBox.Show("올바른 이메일을 입력해주세요!","이메일 오류");
+            }
+            conn = new OleDbConnection(connectionString);
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.CommandText = "select * from 회원 where 아이디 = '" + email + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            OleDbDataReader read = cmd.ExecuteReader();
+            if (!read.Read())
+            {
+                MessageBox.Show("가입 정보가 없습니다!", "비밀번호 재설정불가");
+                if(conn != null)
+                {
+                    conn.Close();
+                }
+                return;
+            }
+
+
+
         }
     }
 }
